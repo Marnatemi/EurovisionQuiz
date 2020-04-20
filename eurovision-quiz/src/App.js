62ab2522af14fe6b771d025c944c310558196c87
Marlena Czerwińska <marlena.stef@gmail.com>
2020-04-20 21:13:43 +0200

    Add data.json

---
+++ eurovision-quiz/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.png';
+import Spiral from './components/Spiral/Spiral'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Spiral />
+    </div>
+  );
+}
+
+export default App;
---
+++ eurovision-quiz/src/components/Spiral/Spiral.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Spiral'
+
+function Spiral() {
+  return (
+    <div className={styles.component}>
+      <path></path>
+      SPIRAL
+    </div>
+  );
+}
+
+export default Spiral;
