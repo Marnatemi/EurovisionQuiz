9b3baee8125735f3cb106fcdabc2891b60468e51
Marlena Czerwińska <marlena.stef@gmail.com>
2020-07-05 12:48:08 +0200

    Add full AnswerCard animation

--- eurovision-quiz/src/components/common/AnswerCard/AnswerCard.js
+++ eurovision-quiz/src/components/common/AnswerCard/AnswerCard.js
@@ -1,113 +1,8 @@
 import React from 'react';
-import {Card, CardContent, CardHeader, Typography} from '@material-ui/core';
+import {Card, CardContent, Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
 const flip = 'flip';
-const flipBack = 'flipBack';
-const growA = 'growA';
-const growB = 'growB';
-const growC = 'growC';
-
-
-// const useStyles = makeStyles(theme => ({
-//   cardWrapper:{
-//     width: 120,
-//     minHeight: 120,
-//     perspective: 120,
-//     bottom: '10%',
-//     left: '50%',
-//     position: 'absolute',
-//     transform: 'translate(-50%, -50%) scale(.6) rotate(90deg)',
-//     transformStyle: 'preserve-3d',
-//     //animation: `$${growB} 5s ease-in-out  forwards`,
-//     '&:first-of-type':{
-//       zIndex: 1,
-//       top: '20%',
-//       left: '50%',
-//       //animation: `$${growA} 5s ease-in-out forwards`,
-//     },
-//     '&:last-of-type':{
-//       //animation: `$${growC} 5s ease-in-out  forwards`,
-//     },
-//     '& *': {
-//       width: '100%',
-//       height: '100%',
-//       backfaceVisibility: 'hidden',
-//     },
-//   },
-//   card: {
-//     position: 'relative',
-//     display: 'flex',
-//     flexDirection: 'column',
-//     justifyContent: 'center',
-//     background: 'linear-gradient(-20deg, rgba(222,180,69,0.7245179063360881) 0%, rgba(240,242,239,0.5) 37%)', 
-//     '& div': {
-//       padding: 0,
-//     },
-//     '& div:last-child' :{
-//       padding: 0,
-//       height: 54,
-//     },
-//     transform: 'rotateY(180deg) rotateZ(90deg)',
-//     animation: `$${flip} 0.8s ease forwards`,
-//     animationDelay: '0.2s',
-//   },
-//   cardBack: {
-//     background: 'linear-gradient(-20deg, rgb(222,180,69) 0%, rgb(240,242,239) 37%)', 
-//     top: 0,
-//     left: 0,
-//     transform: 'rotateY(0deg)',
-//     animation: `$${flipBack} 5s cubic-bezier(.175, .885, .32, 1.275) forwards`,
-//     animationDelay: '0.2s',
-//     position: 'absolute',
-
-//   },
-//   answer: {
-//     position: 'relative',
-//     '& p':{
-//       position: 'absolute',
-//       width: '100%',
-//       top: '50%',
-//       left: '50%',
-//       transform: 'translate(-50%, -50%)',
-//     }
-//   },
-//   [`@keyframes ${flip}`]: {
-//     '100%': {
-//       transform: 'rotateY(0deg) rotateZ(0)',
-
-//     }
-//   },
-//   [`@keyframes ${flipBack}`]: {
-//     '100%': {
-//       transform: 'rotateY(180deg) rotateZ(0)',
-
-      
-//     }
-//   },
-//   [`@keyframes ${growA}`]: {
-//     '100%': {
-//       transform: 'translate(-50%, -50%) scale(1) rotate(0)',
-//       top: '20%',
-//       left: '50%',
-
-//     }
-//   },
-//   [`@keyframes ${growB}`]: {
-//     '100%': {
-//       transform: 'translate(-50%, -50%) scale(1) rotate(0)',
-//       top: '50%',
-//       left: '25%',  
-//     }
-//   },
-//   [`@keyframes ${growC}`]: {
-//     '100%': {
-//       transform: 'translate(-50%, -50%) scale(1) rotate(0)',
-//       top: '50%',
-//       left: '75%',
-//     }
-//   },
-// }));
 
 const useStyles = makeStyles(theme => ({
  cardContainer: {
@@ -118,7 +13,8 @@ const useStyles = makeStyles(theme => ({
    position: 'relative',
    width: '100%',
    height: '100%',
-   animation: `$${flip} 1s ease-in-out forwards`,
+   animation: `$${flip} 0.45s ease-in-out forwards`,
+   animationDelay: '1.2s',
    transformStyle: 'preserve-3d',
    overflow: 'visible',
    '& figure' : {
@@ -134,6 +30,9 @@ const useStyles = makeStyles(theme => ({
      boxShadow: 'inset -1px -1px 7px 0px #0000001f',
 
    },
+   answer: {
+     padding: 0,
+   }
  },
  front: {
   transform: 'rotateY(180deg)',
--- eurovision-quiz/src/components/views/Question/Question.js
+++ eurovision-quiz/src/components/views/Question/Question.js
@@ -4,6 +4,10 @@ import AnswerCard from '../../common/AnswerCard/AnswerCard.js';
 import { makeStyles } from '@material-ui/styles';
 import {Card, CardContent, CardHeader, Chip, Avatar, Modal, Paper} from '@material-ui/core';
 
+const grow = 'grow';
+const growLeft = 'growLeft';
+const growRight = 'growRight';
+
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -29,29 +33,47 @@ const useStyles = makeStyles(theme => ({
     boxShadow: '0px 3px 5px #000',
   },
   answers: {
-    position: 'relative',
     height: '70%',
     margin: 'auto 0',
   },
-  answerCard: {
-    //width: '20%',
-  },
-  chip: {
-    margin: '50px 0 40px',
-    position: 'relative',
-    overflow: 'initial',
-    padding: '20px 0',
-    width: '150px',
-    fontSize: '15px',
-    boxShadow: `0px 3px 5px ${theme.palette.primary.dark}`,
-  },
-  avatar: {
-    background: theme.palette.primary.light,
-    padding: '30px',
-    marginTop: '-55px',
-    fontSize: '30px',
+  answer : {
     position: 'absolute',
-    boxShadow: `0px 3px 5px ${theme.palette.primary.dark}`,
+    left: '50%',
+    bottom: '10%',
+    marginLeft: -60,
+    marginBottom: -75,
+    transform: 'rotate(90deg) scale(0.5)',
+    animation: `$${grow} 0.65s ease-in-out forwards`,
+    animationDelay: '1.2s',
+    '&:first-child': {
+      animation: `$${growLeft} 0.6s ease-in-out forwards`,
+      animationDelay: '1s',
+    },
+    '&:last-child': {
+      animation: `$${growRight} 0.6s ease-in-out forwards`,
+      animationDelay: '1.4s',
+    },
+  },
+  [`@keyframes ${grow}`]: {
+    '100%': {
+      transform: 'rotate(0) scale(1)',
+      bottom: '65%',
+      left: '51.5%'
+    }
+  },
+  [`@keyframes ${growLeft}`]: {
+    '100%': {
+      transform: 'rotate(0) scale(1)',
+      bottom: '45%',
+      left: '28.5%',
+    }
+  },
+  [`@keyframes ${growRight}`]: {
+    '100%': {
+      transform: 'rotate(0) scale(1)',
+      bottom: '40%',
+      left: '71.5%',
+    }
   },
 }));
 
@@ -62,9 +84,9 @@ const demoContent = {
   country: "Francja",
   place: "Londyn (Anglia)",
   answers: [
-    {id: 'A', answer: "Marie Myriam & Celine Dion" },
-    // {id: 'B', answer: "Celine Dion" },
-    // {id: 'C', answer: "Massiel"}
+    {id: 'A', answer: "Marie Myriam" },
+    {id: 'B', answer: "Celine Dion" },
+    {id: 'C', answer: "Massiel"}
   ],
   message: "",
 }
@@ -93,12 +115,13 @@ const Question = () => {
             />
         <CardContent className={classes.answers}>
           {demoContent.answers.map(answer => (
-            <AnswerCard
-              key={answer.id} 
-              className={classes.answerCard}
-              onClick={() => handleClick(answer.answer)}
-              answer={answer.answer}
-            />
+            <div 
+            className={classes.answer}
+            key={answer.id}
+            onClick={() => handleClick(answer.answer)}
+            >
+              <AnswerCard answer={answer.answer}/>
+            </div>
           ))}
           <Modal
             open={open}
