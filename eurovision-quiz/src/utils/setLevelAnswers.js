62d41b9c65944988d694531f7bea8fc023805b98
Marlena Czerwińska <marlena.stef@gmail.com>
2021-01-03 02:02:08 +0100

    Prepare app for deploy

--- eurovision-quiz/src/utils/setLevelAnswers.js
+++ eurovision-quiz/src/utils/setLevelAnswers.js
@@ -1,7 +1,6 @@
 const setLevelAnswers = (level, question, period) => {
   let answerOptions = []
   const randomizedAnswerOptions = []
-  console.log("AT FIRST", 'A:',answerOptions, "RA:", randomizedAnswerOptions)
 
   if (level === 'expert'){
     answerOptions = [question.year]
@@ -10,24 +9,18 @@ const setLevelAnswers = (level, question, period) => {
       const randomYear = Math.floor(Math.random() * (period.to - period.from + 1)) + period.from;
       if(answerOptions.indexOf(randomYear) === -1){
         answerOptions.push(randomYear);
-       console.log("FOR EXPERT", 'A:',answerOptions, "RA:", randomizedAnswerOptions)
-
       } 
     }
 
   } else {
   answerOptions = question[level + 'QuestionOptions']
 }
-console.log("with options", 'A:',answerOptions, "RA:", randomizedAnswerOptions)
-
   // randomize options for all 
   for( let i = 0; i < 3; i++ ){
     const randomElement = Math.floor(Math.random() * (answerOptions.length))
     randomizedAnswerOptions.push(answerOptions[randomElement])
     answerOptions.splice(randomElement, 1)
-    console.log("loop FOR ALL", 'A:',answerOptions, "RA:", randomizedAnswerOptions)
   }
-  console.log(" after all", 'A:',answerOptions, "RA:", randomizedAnswerOptions)
 
   return randomizedAnswerOptions
 }
