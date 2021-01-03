612afddcee2a0b37b21bb158ea04705cc693e6a2
Marlena Czerwińska <marlena.stef@gmail.com>
2021-01-03 03:36:22 +0100

    Updates

---
+++ precache-manifest.9eabda6e10aa1916386f42accd566d41.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f357269d39ed8b07d7b1fd59a191532d",
+    "url": "/EurovisionQuiz/index.html"
+  },
+  {
+    "revision": "d09008310d26df4e8368",
+    "url": "/EurovisionQuiz/static/css/main.57b603c1.chunk.css"
+  },
+  {
+    "revision": "d17f7d96da7fbfd9ee38",
+    "url": "/EurovisionQuiz/static/js/2.52770d7e.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/EurovisionQuiz/static/js/2.52770d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c501518f824d912048",
+    "url": "/EurovisionQuiz/static/js/3.4e7c298c.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/EurovisionQuiz/static/js/3.4e7c298c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09008310d26df4e8368",
+    "url": "/EurovisionQuiz/static/js/main.0215913c.chunk.js"
+  },
+  {
+    "revision": "37a312dae07c48276e10",
+    "url": "/EurovisionQuiz/static/js/runtime-main.05b8ae84.js"
+  },
+  {
+    "revision": "793f08cf8c7f6a2257eb11595985640e",
+    "url": "/EurovisionQuiz/static/media/Intro.793f08cf.mp3"
+  },
+  {
+    "revision": "47193629a54f3138d190cd6a9c10147e",
+    "url": "/EurovisionQuiz/static/media/SaxBeep.47193629.mp3"
+  },
+  {
+    "revision": "c369277a096d791d29d82091ae5b2f69",
+    "url": "/EurovisionQuiz/static/media/SaxHonk.c369277a.mp3"
+  },
+  {
+    "revision": "5b4a34e924eceeee794c4f0abf1451ea",
+    "url": "/EurovisionQuiz/static/media/alert.5b4a34e9.mp3"
+  },
+  {
+    "revision": "39dcd6691aba6480f03e65ab8be99b37",
+    "url": "/EurovisionQuiz/static/media/applause.39dcd669.mp3"
+  },
+  {
+    "revision": "1c6e8102a3d2f239b355ff41f5a61012",
+    "url": "/EurovisionQuiz/static/media/cardOpen.1c6e8102.mp3"
+  },
+  {
+    "revision": "30cd3bd5a008634d2556d60db04ed896",
+    "url": "/EurovisionQuiz/static/media/click1.30cd3bd5.mp3"
+  },
+  {
+    "revision": "3300ec642e2ac7b3889768d99536e1e5",
+    "url": "/EurovisionQuiz/static/media/click3.3300ec64.mp3"
+  },
+  {
+    "revision": "fd4460224e5d75b62b8b37651a2d912d",
+    "url": "/EurovisionQuiz/static/media/coin.fd446022.mp3"
+  }
+]);
